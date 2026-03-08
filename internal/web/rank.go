42254f3295dd5457130afda863d314bddf8abba5
fssae <2973387251@qq.com>
2026-03-08 15:22:29 +0800

    new

--- internal/web/rank.go
+++ internal/web/rank.go
@@ -20,8 +20,8 @@ func (h *TeacherHandler) GetRank(c *gin.Context) {
 	// 去掉classname中的"班"字
 	if req.ClassName != "" {
 		req.ClassName = strings.TrimSuffix(req.ClassName, "班")
+		req.ClassName = req.ClassName + " 班"
 	}
-	req.ClassName = req.ClassName + " 班"
 	if req.Page == 0 || req.PageSize == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"code": 400,
